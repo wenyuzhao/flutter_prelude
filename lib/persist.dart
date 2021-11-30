946962f7a39f2d1f61c940904e24e5539109b13a
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-11-30 21:53:35 +1100

    Add json libs

--- lib/persist.dart
+++ lib/persist.dart
@@ -1,3 +1,4 @@
 export 'package:path/path.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:sqflite/sqflite.dart';
+export 'package:json_annotation/json_annotation.dart';
